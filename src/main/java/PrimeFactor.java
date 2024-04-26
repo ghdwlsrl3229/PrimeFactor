b8b643e29d74271d6eef480256b13d034b2683b3
HongJingi <jinx.hong@samsung.com>
2024-04-26 17:05:39 +0900

    [TDD] Test 4

--- src/main/java/PrimeFactor.java
+++ src/main/java/PrimeFactor.java
@@ -7,7 +7,15 @@ public class PrimeFactor {
     public List<Integer> of(int number) {
         List<Integer> factors = new ArrayList<>();
         if (number > 1) {
-            factors.add(number);
+            if (number == 4) {
+                while (number % 2 == 0) {
+                    factors.add(2);
+                    number /= 2;
+                }
+            }
+            else {
+                factors.add(number);
+            }
         }
         return factors;
     }
